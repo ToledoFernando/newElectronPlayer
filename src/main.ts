4e01622fac7c9816f77481cb4d2825e0ba8f6a63
toledofernando <toledof764@gmail.com>
2023-05-07 05:20:35 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+let mainWindow: Electron.BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "client", "index.html"));
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
