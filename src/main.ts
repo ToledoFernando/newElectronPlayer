117788d0466222d65c695a7db4b076dfdb53e01d
toledofernando <toledof764@gmail.com>
2023-05-08 09:13:02 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,26 @@ import path from "path";
 
 let mainWindow: Electron.BrowserWindow | null;
 
+let isDev = false;
+
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 850,
+    minWidth: 850,
+    height: 650,
+    minHeight: 650,
+    // transparent: true,
+    // backgroundColor: "#00000000",
+    // frame: false,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, "client", "index.html"));
+  isDev
+    ? mainWindow.loadURL("http://localhost:5173")
+    : mainWindow.loadFile(path.join(__dirname, "client", "index.html"));
+  // mainWindow.loadFile("dist/client/index.html");
 
   mainWindow.on("closed", () => {
     mainWindow = null;
